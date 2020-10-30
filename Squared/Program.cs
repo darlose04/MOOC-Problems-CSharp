3f8b886170d7dc651a8f1ecc21d8a1cfddcc2458
Zach Smith <zsmith126028@gmail.com>
2020-10-30 12:31:26 -0500

    add Squared to git

---
+++ Squared/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+namespace Squared
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Enter a number: ");
+            int number = Int32.Parse(Console.ReadLine());
+
+            int square = number * number;
+
+            Console.WriteLine($"The square of {number} is {square}.");
+        }
+    }
+}
