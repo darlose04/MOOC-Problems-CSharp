32c87c8637a1ddf94139a75b6a7d6c3e73e3f514
Zach Smith <zsmith126028@gmail.com>
2020-11-10 13:40:22 -0600

    add problem to compare two numbers

---
+++ CompareNumbers/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+
+namespace CompareNumbers
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Please enter a number: ");
+            var x = Int32.Parse(Console.ReadLine());
+
+            Console.WriteLine("Please enter a second number: ");
+            var y = Int32.Parse(Console.ReadLine());
+
+            CompareTwoNumbers(x, y);
+        }
+
+        public static void CompareTwoNumbers(int x, int y)
+        {
+            if (x > y)
+            {
+                Console.WriteLine($"{x} is greater than {y}");
+            } else if (x < y)
+            {
+                Console.WriteLine($"{x} is smaller than {y}");
+            } else
+            {
+                Console.WriteLine($"{x} is equal to {y}");
+            }
+        }
+    }
+}
