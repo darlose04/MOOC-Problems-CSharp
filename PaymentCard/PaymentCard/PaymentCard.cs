3d65ed25b151c3cc3bd33ddfdb3474d6a00a7085
Zach Smith <zsmith126028@gmail.com>
2021-01-14 11:34:47 -0600

    add new problem in different project

---
+++ PaymentCard/PaymentCard/PaymentCard.cs
@@ -0,0 +1,64 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace PaymentCard
+{
+    public class PaymentCard
+    {
+        private double _balance { get; set; }
+
+        public PaymentCard(double openingBalance)
+        {
+            _balance = openingBalance;
+        }
+
+        public void EatAffordably()
+        {
+            if (_balance - 2.60 < 0)
+            {
+                _balance -= 0;
+            }
+            else
+            {
+                _balance -= 2.60;
+            }
+        }
+
+        public void EatHeartily()
+        {
+            if (_balance - 4.60 < 0)
+            {
+                _balance -= 0;
+            }
+            else
+            {
+                _balance -= 4.60;
+            }
+        }
+
+        public void AddMoney(double amount)
+        {
+            if (amount < 0)
+            {
+                _balance += 0;
+            }
+            else
+            {
+                if (_balance + amount > 150.0)
+                {
+                    _balance = 150.0;
+                }
+                else
+                {
+                    _balance += amount;
+                }
+            }
+        }
+
+        public override string ToString()
+        {
+            return $"The card has a balance of {_balance} euros.";
+        }
+    }
+}
---
+++ PaymentCard/PaymentCard/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+
+namespace PaymentCard
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            PaymentCard card = new PaymentCard(10);
+            Console.WriteLine(card);
+
+            card.AddMoney(15);
+            Console.WriteLine(card);
+
+            card.EatAffordably();
+            Console.WriteLine(card);
+
+            card.EatHeartily();
+            Console.WriteLine(card);
+
+            card.AddMoney(200);
+            Console.WriteLine(card);
+
+            card.AddMoney(-50);
+            Console.WriteLine(card);
+        }
+    }
+}
