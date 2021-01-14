6b67254d707cb5c5c3e5421c780c6f775a12b161
Zach Smith <zsmith126028@gmail.com>
2021-01-14 10:15:43 -0600

    add new problems dir

---
+++ PartFourProblems/PartFourProblems/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using Statistics;
+
+namespace PartFourProblems
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Statistics.Statistics statistics = new Statistics.Statistics();
+            statistics.AddNumber(3);
+            statistics.AddNumber(5);
+            statistics.AddNumber(6);
+            statistics.AddNumber(1);
+            statistics.AddNumber(2);
+            Console.WriteLine($"Count: {statistics.Count}");
+        }
+    }
+}
---
+++ PartFourProblems/Statistics/Statistics.cs
@@ -0,0 +1,30 @@
+﻿using System;
+
+namespace Statistics
+{
+    public class Statistics
+    {
+        private int _count { get; set; }
+        public int Count
+        {
+            get
+            {
+                return _count;
+            }
+            set
+            {
+                _count = value;
+            }
+        }
+
+        public Statistics()
+        {
+            this._count = 0;
+        }
+
+        public void AddNumber(int number)
+        {
+            this._count++;
+        }
+    }
+}
